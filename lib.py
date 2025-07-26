ffeaec03dd1ef1af1f28e173747bad31263c936f
Robin Beer <robin.beer@flexa.energy>
2025-07-26 16:11:33 +0200

    implementation of simplistic function

---
+++ lib.py
@@ -0,0 +1,14 @@
+def sum_objects(a, b):
+    """Sum two objects together.
+
+    Args:
+        a: First object to sum
+        b: Second object to sum
+
+    Returns:
+        The sum of a and b
+
+    Raises:
+        TypeError: If the objects cannot be summed
+    """
+    return a + b
